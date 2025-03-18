ec08071bf22664aab80ac2dc249c128e099f6db9
leooon3 <riccardo.leone@studenti.unitn.it>
2025-03-18 14:28:27 +0100

    brush_wroking

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ var el = document.getElementById('c');
 var ctx = el.getContext('2d');
 var isDrawing = false;
 
+// Set stroke properties for better visibility
+ctx.strokeStyle = "black";  // Line color
+ctx.lineWidth = 2;          // Line thickness
+ctx.lineJoin = "round";     // Smooth joints
+ctx.lineCap = "round";      // Rounded stroke edges
+
+function getMousePos(e) {
+  let rect = el.getBoundingClientRect();
+  let scaleX = el.width / rect.width;  // Scale factor in X
+  let scaleY = el.height / rect.height; // Scale factor in Y
+  return {
+    x: (e.clientX - rect.left) * scaleX,
+    y: (e.clientY - rect.top) * scaleY
+  };
+}
+
 el.onmousedown = function(e) {
   isDrawing = true;
-  ctx.beginPath(); // Start a new path
-  let rect = el.getBoundingClientRect();
-  ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
+  let pos = getMousePos(e);
+  ctx.beginPath();
+  ctx.moveTo(pos.x, pos.y);
 };
 
 el.onmousemove = function(e) {
   if (isDrawing) {
-    let rect = el.getBoundingClientRect();
-    ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
+    let pos = getMousePos(e);
+    ctx.lineTo(pos.x, pos.y);
     ctx.stroke();
   }
 };
@@ -20,10 +36,11 @@ el.onmousemove = function(e) {
 el.onmouseup = function() {
   isDrawing = false;
 };
-el.onmouseleave = function() { 
+/*el.onmouseleave = function() { 
   isDrawing = false; 
-};
+};*/
 
+// Function to clear the canvas
 document.getElementById('clearBtn').onclick = function() {
-    ctx.clearRect(0, 0, el.width, el.height);
-  };
+  ctx.clearRect(0, 0, el.width, el.height);
+};
