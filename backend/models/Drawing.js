8b13cc835f305e43aa27a7a8cfcdcfd1bac961d0
Rick <leooonericcardo@gmail.com>
2025-04-02 12:34:50 +0100

    cose

--- backend/models/Drawing.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DrawingSchema = new mongoose.Schema({
-  userId: String,
-  imageData: String, // Save drawing as base64 string
-});
-
-module.exports = mongoose.model("Drawing", DrawingSchema);
